462a20a37a8f00add42df09bcc5448c09f4b7703
Nicholas Soper <nick@rocketmakers.com>
2019-02-18 19:56:43 +0000

    Icon credit text

--- src/components/IconCredit.js
+++ src/components/IconCredit.js
@@ -4,28 +4,30 @@ import { ExternalLink } from './ExternalLink'
 
 import './IconCredit.scss'
 
-const authors = [{
-  name: 'Freepik',
-  url: 'https://www.freepik.com/'
-}]
+const authors = [
+  {
+    name: 'Freepik',
+    url: 'https://www.freepik.com/',
+  },
+]
 
-export function IconCredit() { 
+export function IconCredit() {
   return (
     <div className="icon-credit">
-    Icons made by
-    {authors.map(a => (
-      <ExternalLink href={a.url} title={a.name}>
-        {a.name}
+      Icons made by
+      {authors.map(a => (
+        <ExternalLink href={a.url} title={a.name}>
+          {a.name}
+        </ExternalLink>
+      ))}
+      from
+      <ExternalLink href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
       </ExternalLink>
-    ))}
-    from
-    <ExternalLink href="https://www.flaticon.com/" title="Flaticon">
-      www.flaticon.com
-    </ExternalLink>
-    is licensed by
-    <ExternalLink href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">
-    CC 3.0 BY
-    </ExternalLink>
+      licensed by
+      <ExternalLink href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">
+        CC 3.0 BY
+      </ExternalLink>
     </div>
   )
 }
