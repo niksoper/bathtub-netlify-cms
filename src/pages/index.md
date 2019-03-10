68448ba2a948609cb1fdba2c7b44587871b09eac
Nicholas Soper <nick@rocketmakers.com>
2019-03-10 14:45:33 +0000

    Logo on home page

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,9 +1,8 @@
 ---
 templateKey: 'index-page'
 title: 'BathTub Orchestra'
-image: /img/2019-dec-performance-front-LO-RES.jpg
+image: /img/logo-man-in-bath.jpg
 heading: Bath’s newest community orchestra
 description: >-
   Founded in October 2018, the Bath Tub Orchestra is a mixed ability orchestra that plays all kinds of music from pop songs, jazz , blues, film soundtracks and classical favourites.
 ---
-
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,37 +4,13 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 
+import './index-page.scss'
+
 export const IndexPageTemplate = ({ image, title, heading, description }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${!!image.childImageSharp ? image.childImageSharp.fluid.src : image})`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow: 'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
+    <div className="jumbotron">
+      <div className="logo-wrap">
+        <img alt="BathTub Orchestra logo" src={!!image.childImageSharp ? image.childImageSharp.fluid.src : image} />
       </div>
     </div>
     <section className="section section--gradient">
