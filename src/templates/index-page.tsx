449060eac779f9d425ab14cd0ea8accbd8d18e9d
Nick Soper <nick@rocketmakers.com>
2022-03-30 19:17:40 +0100

    Remove St Swithin's concert from the home page

--- src/templates/index-page.tsx
+++ src/templates/index-page.tsx
@@ -34,10 +34,6 @@ export const IndexPageTemplate = ({
               </div>
             </div>
             <div className="column is-12">
-              <h3 className="has-text-weight-semibold is-size-2">Our next performance</h3>
-              <NextConcert />
-            </div>
-            <div className="column is-12">
               <h3 className="has-text-weight-semibold is-size-2">Latest stories</h3>
               <BlogRoll />
               <div className="column is-12 has-text-centered">
